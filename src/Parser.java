89354d34ada0c89db5fe924c9a239f2d83766451
João Monteiro <joaomiguelmonteiro@gmail.com>
2017-02-13 20:24:02 +0000

    First commit

---
+++ src/Parser.java
@@ -0,0 +1,22 @@
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Parser {
+    private Workbook input;
+
+    public static void main(String [] args)
+    {
+        importExcel();
+    }
+
+    private static void importExcel()
+    {
+
+    }
+}
