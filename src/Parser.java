69d515735fe6564346a852b62709e0a88b346b5f
João Monteiro <joaomiguelmonteiro@gmail.com>
2017-02-14 18:18:16 +0000

    Merging excel files

--- src/Parser.java
+++ src/Parser.java
@@ -1,22 +1,150 @@
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.xssf.usermodel.*;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Parser {
-    private Workbook input;
+    private static String file1Path = "HealthTalks- recetividade [FB] (Respostas).xlsx";
+    private static String file2Path = "HealthTalks- recetividade [SI] (Respostas).xlsx";
+    private static String concatPath = "Result.xlsx";
 
-    public static void main(String [] args)
-    {
-        importExcel();
+    public static void main(String [] args) throws IOException {
+        File mergedDoc = new File(concatPath);
+        FileInputStream doc1 = new FileInputStream(file1Path);
+        FileInputStream doc2 = new FileInputStream(file2Path);
+        List<FileInputStream> docsList = Arrays.asList(doc1, doc2);
+        mergeExcelFiles(mergedDoc, docsList);
+/*
+        try {
+            OPCPackage concat = OPCPackage.open(new File(concatPath));
+            XSSFWorkbook concatWB = new XSSFWorkbook(concat);
+
+
+
+            concat.close();
+        } catch (IOException | InvalidFormatException e) {
+            e.printStackTrace();
+        }
+*/
+    }
+
+    /**
+     * Credit to David Sommer @ https://gist.github.com/davidsommer/7805574
+     *
+     * @param file resulting file
+     * @param list list of files to merge
+     * @throws IOException
+     */
+    private static void mergeExcelFiles(File file, List<FileInputStream> list) throws IOException {
+        XSSFWorkbook book = new XSSFWorkbook();
+        XSSFSheet sheet = book.createSheet(file.getName());
+
+        for (FileInputStream fin : list) {
+            XSSFWorkbook b = new XSSFWorkbook(fin);
+            for (int i = 0; i < b.getNumberOfSheets(); i++) {
+                copySheets(book, sheet, b.getSheetAt(i));
+            }
+        }
+
+        try {
+            writeFile(book, file);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeFile(XSSFWorkbook book, File file) throws Exception {
+        FileOutputStream out = new FileOutputStream(file);
+        book.write(out);
+        out.close();
+    }
+
+    private static void copySheets(XSSFWorkbook newWorkbook, XSSFSheet newSheet, XSSFSheet sheet){
+        copySheets(newWorkbook, newSheet, sheet, true);
+    }
+
+    private static void copySheets(XSSFWorkbook newWorkbook, XSSFSheet newSheet, XSSFSheet sheet, boolean copyStyle){
+        int newRownumber = newSheet.getLastRowNum() + 1;
+        int maxColumnNum = 0;
+        Map<Integer, XSSFCellStyle> styleMap = (copyStyle) ? new HashMap<Integer, XSSFCellStyle>() : null;
+
+        for (int i = sheet.getFirstRowNum(); i <= sheet.getLastRowNum(); i++) {
+            XSSFRow srcRow = sheet.getRow(i);
+            XSSFRow destRow = newSheet.createRow(i + newRownumber);
+            if (srcRow != null) {
+                copyRow(newWorkbook, sheet, newSheet, srcRow, destRow, styleMap);
+                if (srcRow.getLastCellNum() > maxColumnNum) {
+                    maxColumnNum = srcRow.getLastCellNum();
+                }
+            }
+        }
+        for (int i = 0; i <= maxColumnNum; i++) {
+            newSheet.setColumnWidth(i, sheet.getColumnWidth(i));
+        }
+    }
+
+    private static void copyRow(XSSFWorkbook newWorkbook, XSSFSheet srcSheet, XSSFSheet destSheet, XSSFRow srcRow, XSSFRow destRow, Map<Integer, XSSFCellStyle> styleMap) {
+        destRow.setHeight(srcRow.getHeight());
+        for (int j = srcRow.getFirstCellNum(); j <= srcRow.getLastCellNum(); j++) {
+            XSSFCell oldCell = srcRow.getCell(j);
+            XSSFCell newCell = destRow.getCell(j);
+            if (oldCell != null) {
+                if (newCell == null) {
+                    newCell = destRow.createCell(j);
+                }
+                copyCell(newWorkbook, oldCell, newCell, styleMap);
+            }
+        }
+    }
+
+    private static void copyCell(XSSFWorkbook newWorkbook, XSSFCell oldCell, XSSFCell newCell, Map<Integer, XSSFCellStyle> styleMap) {
+        if(styleMap != null) {
+            int stHashCode = oldCell.getCellStyle().hashCode();
+            XSSFCellStyle newCellStyle = styleMap.get(stHashCode);
+            if(newCellStyle == null){
+                newCellStyle = newWorkbook.createCellStyle();
+                newCellStyle.cloneStyleFrom(oldCell.getCellStyle());
+                styleMap.put(stHashCode, newCellStyle);
+            }
+            newCell.setCellStyle(newCellStyle);
+        }
+        switch(oldCell.getCellType()) {
+            case XSSFCell.CELL_TYPE_STRING:
+                newCell.setCellValue(oldCell.getRichStringCellValue());
+                break;
+            case XSSFCell.CELL_TYPE_NUMERIC:
+                newCell.setCellValue(oldCell.getNumericCellValue());
+                break;
+            case XSSFCell.CELL_TYPE_BLANK:
+                newCell.setCellType(XSSFCell.CELL_TYPE_BLANK);
+                break;
+            case XSSFCell.CELL_TYPE_BOOLEAN:
+                newCell.setCellValue(oldCell.getBooleanCellValue());
+                break;
+            case XSSFCell.CELL_TYPE_ERROR:
+                newCell.setCellErrorValue(oldCell.getErrorCellValue());
+                break;
+            case XSSFCell.CELL_TYPE_FORMULA:
+                newCell.setCellFormula(oldCell.getCellFormula());
+                break;
+            default:
+                break;
+        }
     }
 
     private static void importExcel()
     {
-
+        /*
+        try {
+            Workbook input = WorkbookFactory.create(new File("MyExcel.xls"));
+        } catch (IOException | InvalidFormatException e) {
+            e.printStackTrace();
+        }
+        */
     }
 }
